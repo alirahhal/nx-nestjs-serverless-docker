397b9a05cb4e20e0a3f14f05a66cd8e7cb543361
Ali Rahhal <arahhal997@gmail.com>
2022-08-13 18:15:26 +0300

    Lambda docker deployment

--- apps/cdk-app/lib/cdk-app-stack.ts
+++ apps/cdk-app/lib/cdk-app-stack.ts
@@ -12,26 +12,24 @@ export class CdkAppStack extends Stack {
   }
 
   private createBackendNestServerlessLambda() {
-    // const fn = new lambda.DockerImageFunction(this, 'new-controller-docker', {
-    //     functionName: 'new-controller-docker',
-    //     logRetention: logs.RetentionDays.ONE_MONTH,
-    //     timeout: Duration.seconds(10),
-    //     code: lambda.DockerImageCode.fromImageAsset("../../", {
-    //         file: "apps/my-nest-app/Dockerfile.Lambda",
-    //     }),
-    //     environment: {
-    //         ENV: "staging"
-    //     }
-    // })
+    const fn = new lambda.DockerImageFunction(this, 'NestServerlessLambdaDocker', {
+      functionName: 'nest-serverless-lambda-docker',
+      code: lambda.DockerImageCode.fromImageAsset("../../", {
+        file: "apps/my-nest-app/Dockerfile.Lambda",
+        exclude: [
+          'apps/cdk-app/cdk.out',
+        ],
+      })
+    })
 
-    const fn = new lambda.Function(this, "NestServerlessLambda", {
-      functionName: "nest-serverless-lambda",
-      runtime: lambda.Runtime.NODEJS_14_X,
-      handler: "lambda.handler",
-      code: lambda.Code.fromAsset(
-        "../../dist/apps/my-nest-app/"
-      ),
-    });
+    // const fn = new lambda.Function(this, "NestServerlessLambda", {
+    //   functionName: "nest-serverless-lambda",
+    //   runtime: lambda.Runtime.NODEJS_14_X,
+    //   handler: "lambda.handler",
+    //   code: lambda.Code.fromAsset(
+    //     "../../dist/apps/my-nest-app/"
+    //   ),
+    // });
 
     new apigw.LambdaRestApi(this, "MyApi", {
       handler: fn,
